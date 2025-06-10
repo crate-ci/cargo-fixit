7547d8e381df25015d7f0be49c94bbb0aaa587b6
Pyrode <pyrode.01@gmail.com>
2025-06-10 13:08:55 +0530

    refactor: Renamed `Fixit` to `FixitArgs`

--- src/bin/cargo-fixit/fixit.rs
+++ src/bin/cargo-fixit/fixit.rs
@@ -2,13 +2,13 @@ use cargo_fixit::CargoResult;
 use clap::Parser;
 
 #[derive(Debug, Parser)]
-pub(crate) struct Fixit {
+pub(crate) struct FixitArgs {
     /// Unstable (nightly-only) flags
     #[arg(short = 'Z', value_name = "FLAG")]
     unstable_flags: Vec<String>,
 }
 
-impl Fixit {
+impl FixitArgs {
     pub(crate) fn exec(self) -> CargoResult<()> {
         anyhow::bail!("not implemented");
     }
