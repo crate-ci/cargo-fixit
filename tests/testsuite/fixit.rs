8ac50fe7a6356427a283af777c15aa9f6b664e5d
Pyrode <pyrode.01@gmail.com>
2025-06-13 20:08:31 +0530

    test: Added test case for unfixable error

--- tests/testsuite/fixit.rs
+++ tests/testsuite/fixit.rs
@@ -46,3 +46,58 @@ fn basic() {
 "#]],
     );
 }
+
+#[cargo_test]
+fn fixable_and_unfixable() {
+    let p = project()
+        .file(
+            "src/lib.rs",
+            r#"
+            pub fn a() {
+                let mut b = 10;
+                let _ = b;
+
+                let c = 10;
+            }
+            "#,
+        )
+        .build();
+
+    p.cargo_("fixit")
+        .with_status(0)
+        .with_stderr_data(str![[r#"
+[WARNING] unused variable: `c`
+ --> src/lib.rs:6:21
+  |
+6 |                 let c = 10;
+  |                     ^ [HELP] if this is intentional, prefix it with an underscore: `_c`
+  |
+  = [NOTE] `#[warn(unused_variables)]` on by default
+
+[WARNING] variable does not need to be mutable
+ --> src/lib.rs:3:21
+  |
+3 |                 let mut b = 10;
+  |                     ----^
+  |                     |
+  |                     [HELP] remove this `mut`
+  |
+  = [NOTE] `#[warn(unused_mut)]` on by default
+
+
+"#]])
+        .run();
+    assert_ui().eq(
+        p.read_file("src/lib.rs"),
+        str![[r#"
+
+            pub fn a() {
+                let mut b = 10;
+                let _ = b;
+
+                let c = 10;
+            }
+            
+"#]],
+    );
+}
